ff921d21a9b6740e51988e8fe1bc4073bdfeb4ef
tam131 <tcy6247@gmail.com>
2025-12-25 21:20:54 +0800

    没有返回值的方法

---
+++ 20251225/Test1225_7.java
@@ -0,0 +1,15 @@
+/**
+ * 没有返回值的方法
+ */
+
+public class Test1225_7 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        print(a,b);
+    }
+
+    public static void print(int x,int y){
+        System.out.println("x = "+x+" y = "+y);
+    }
+}
