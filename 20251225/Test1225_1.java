8c64f3f1dea5aa8fc77f26da6a8c6e5b25292f74
tam131 <tcy6247@gmail.com>
2025-12-25 21:18:35 +0800

    方法的调用1

---
+++ 20251225/Test1225_1.java
@@ -0,0 +1,15 @@
+/**
+ * 方法的的调用1
+ */
+public class Test1225_1 {
+    public static  void add(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+//            add(1,3);
+            int x = 5;
+            int y = 8;
+            add(x,y);//调用方法
+    }
+}
+
